04d2dbe0854badb87f30bb398bbfce773d3e9455
Jakub Bouček <pan@jakubboucek.cz>
2019-06-27 16:31:50 +0200

    Improve API 404 handling

--- src/server.js
+++ src/server.js
@@ -6,11 +6,15 @@ const port = 8080;
 
 app.use(cors());
 
-app.get('/events/:id', async (req, res) => {
+app.get('/events/:id?', async (req, res) => {
     if (!req.params.id) {
-        return res.status(400).json({status: 'error', message: 'You should provide page id.'})
+        return res.status(400).json({status: 'error', message: 'You should provide Facebook page id. Use: /event/{pageid}'});
     }
     res.json({status: 'ok', data: await getFbEvents(req.params.id)});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Send API 404
+app.use(function (req, res) {
+    res.status(400).json({status: 'error', message: 'Endpoint not found.'});
+});
