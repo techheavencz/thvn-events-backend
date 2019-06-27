8ad8638202f4b432a62dc0c04af38efc503ef80b
Jakub Bouček <pan@jakubboucek.cz>
2019-06-28 01:41:45 +0200

    Add server cache to keep responses 1 hour

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 const TEST_PAGE_ID = '1777732895836985';
 
-const getFbEvents = require('./getFbEvents.js');
+const getFbEvents = require('./fb-events-fetcher.js');
 
 async function main() {
     const events = await getFbEvents(TEST_PAGE_ID);
