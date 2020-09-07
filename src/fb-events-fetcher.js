2ec8666f7acd28c8705f94b20dddab8433ece8af
Jakub Bouček <pan@jakubboucek.cz>
2020-09-07 09:08:02 +0200

    Fb fetcher: Update to FB API v8.0
    No changes in their request call

--- src/fb-events-fetcher.js
+++ src/fb-events-fetcher.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 const FB_GRAPH_API_URL = `https://graph.facebook.com`;
-const FB_GRAPH_API_VERSION = `v3.3`;
+const FB_GRAPH_API_VERSION = `v8.0`;
 const FB_GRAPH_API_PAGE_TOKEN = `EAAJTkK7oiPABAE8nLfRqL3MhhiqSZAyaViVrXwvlJFpGxPPWYRo0DseS6uFIqiYZBWkQNgqa50XEoYZAVZCZCboC7awLvwFE7UkkrCCg1Dc4ENYrLum1oRZC07NV9J3uw36PQsW7L23v7fotG4k4ou4dOuKX0ZB05PH8SrNVapPbwZDZD`;
 const FB_GRAPH_API_EVENTS_QUERY = `events{id,name,description,start_time,cover,ticket_uri,place}`;
 const FB_URL = `https://www.facebook.com`;
